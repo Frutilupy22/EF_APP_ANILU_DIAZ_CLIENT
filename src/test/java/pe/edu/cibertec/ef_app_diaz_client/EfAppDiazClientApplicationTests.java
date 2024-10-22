0ab64185ced5c22bfa1c93c3b8679aa1a8886ff4
Frutilupy22 <camiladiaz_0322@hotmail.com>
2024-10-22 05:23:21 -0500

    ....

---
+++ src/test/java/pe/edu/cibertec/ef_app_diaz_client/EfAppDiazClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.ef_app_diaz_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfAppDiazClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
