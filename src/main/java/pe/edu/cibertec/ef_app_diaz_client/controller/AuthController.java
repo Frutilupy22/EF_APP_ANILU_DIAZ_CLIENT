255b94259b07921a7975f2eb2d3ea226fade6451
Frutilupy22 <camiladiaz_0322@hotmail.com>
2024-10-22 06:07:13 -0500

    a

--- src/main/java/pe/edu/cibertec/ef_app_diaz_client/controller/AuthController.java
+++ src/main/java/pe/edu/cibertec/ef_app_diaz_client/controller/AuthController.java
@@ -1,9 +1,6 @@
 package pe.edu.cibertec.ef_app_diaz_client.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
